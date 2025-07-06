12e0b82617d4392c63a3988f5a34422cfc80b3ba
Kalputra <gregethaikal@gmail.com>
2025-07-06 21:29:30 +0700

    sudah masuk

--- script/script.js
+++ script/script.js
@@ -1,3 +1,11 @@
+// Selamat datang
+        window.onload = function() {
+            if (!localStorage.getItem("sudahMasuk")) {
+                alert("Selamat datang di website gw!");
+                localStorage.setItem("sudahMasuk", true);
+            }
+        }
+
 //formbutton
 function formbutton() {
 
