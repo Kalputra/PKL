e3582f96e606cd2e619a765dc30d21e85e3fef9f
rfaa9809 <rfafa0989@gmail.com>
2025-07-06 23:44:54 +0700

    smoth Scroll

--- script/script.js
+++ script/script.js
@@ -1,33 +1,3 @@
-//formbutton
-const form = document.getElementById('contact');
-
-form.addEventListener("submit", function(e){
-    e.preventDefault();
-
-    const nama = document.getElementById('nama').value;
-    const email = document.getElementById('email').value;
-    const telp = document.getElementById('telp').value;
-    const gender = document.querySelector('input[name="gender"]:checked')?.value;
-
-    if (!nama || !email || !telp || !gender) {
-        alert("Harap Isi Semua Data!");
-        return;
-    }
-
-    const dataUser = {
-        nama: nama,
-        email: email,
-        telp: telp,
-        gender: gender
-    };
-
-    localStorage.setItem(dataUser,JSON.stringify(dataUser));
-
-    alert("Data Berhasil disimpan di localStorage!");
-    form.reset();
-
-}); 
-
 //active link
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
